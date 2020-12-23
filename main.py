1e61b801a6ea0455362f620a40e40963b74c125a
DavidHernandez21 <55497518+DavidHernandez21@users.noreply.github.com>
2020-12-23 20:39:28 +0100

    Update main.py
    fix import naming bugs

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import rc
-from Scripts.main_laplace_2d import main as main_laplace_2d
-from Scripts.main_laplace_nd import main as main_laplace_nd
+from Scripts_tf2.main_laplace_2d import main as main_laplace_2d
+from Scripts_tf2.main_laplace_nd import main as main_laplace_nd
 
 plt.rcParams.update({'font.size': 16})
 rc('text', usetex=True)
